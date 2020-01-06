2e5e694a67b29558b50d64504253fbff84132a31
Gorbataras <gorbataras@gmail.com>
2020-01-06 14:45:54 -0500

    added print array

--- index.php
+++ index.php
@@ -17,5 +17,22 @@ ini_set("display_errors", 1);
 </head>
 <body>
 <h1>Pair Program 1</h1>
+<?php
+
+$numbers = array(7, 9, 8, 9, 8, 8, 6);
+
+printArr($numbers);
+
+
+function printArr($arr) {
+    foreach ($arr as $value) {
+        echo $value;
+        echo "<br>";
+    }
+}
+
+
+?>
+
 </body>
 </html>
